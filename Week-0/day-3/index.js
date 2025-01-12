b32fb719a2d43807f80cbb598a00be2cc40e0cf1
devUmar91 <umree112233@gmail.com>
2025-01-12 23:33:17 +0500

    higherOrder fn

---
+++ Week-0/day-3/index.js
@@ -0,0 +1,16 @@
+const add = (a,b)=>{
+    let sum=a+b
+    console.log(sum);
+    
+     return sum
+}
+
+const calculate = (add)=>{
+
+    let a = 3;
+    let b= 4;
+     add(a,b)
+    
+}
+
+calculate(add)
