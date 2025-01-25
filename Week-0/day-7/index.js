74246482554a3d7d98e63c20fcc4671771b57ab1
devUmar91 <umree112233@gmail.com>
2025-01-25 23:56:44 +0500

    .

--- Week-0/day-7/index.js
+++ Week-0/day-7/index.js
@@ -210,7 +210,62 @@ const task = (id) => new Promise((resolve) => {
   }, 1000);
 });
 
-queue.add(() => task(1));
-queue.add(() => task(2));
-queue.add(() => task(3));
-queue.add(() => task(4));
+// queue.add(() => task(1));
+// queue.add(() => task(2));
+// queue.add(() => task(3));
+// queue.add(() => task(4));
+
+// ....Spiral Matrix..
+
+function spiralOrder(matrix) {
+    if (matrix.length === 0) return [];
+  
+    const result = [];
+    let top = 0,
+      bottom = matrix.length - 1,
+      left = 0,
+      right = matrix[0].length - 1;
+  
+    while (top <= bottom && left <= right) {
+      // Traverse from left to right
+      for (let i = left; i <= right; i++) {
+        result.push(matrix[top][i]);
+      }
+      top++;
+  
+      // Traverse from top to bottom
+      for (let i = top; i <= bottom; i++) {
+        result.push(matrix[i][right]);
+      }
+      right--;
+  
+      if (top <= bottom) {
+        // Traverse from right to left
+        for (let i = right; i >= left; i--) {
+          result.push(matrix[bottom][i]);
+        }
+        bottom--;
+      }
+  
+      if (left <= right) {
+        // Traverse from bottom to top
+        for (let i = bottom; i >= top; i--) {
+          result.push(matrix[i][left]);
+        }
+        left++;
+      }
+    }
+  
+    return result;
+  }
+  
+  // Example Usage:
+  const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  
+  console.log(spiralOrder(matrix));
+  // Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+  
